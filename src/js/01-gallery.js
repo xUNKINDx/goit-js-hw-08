e24f9a09c587ebaf0319d1a60d02ec3d4966e58b
Chumak Slavik <chumakslav@gmail.com>
2023-01-09 23:50:47 +0200

    1st hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(photo => 
+    `<a class="gallery__item" href="${photo.original}">
+        <img class="gallery__image" src="${photo.preview}" alt="${photo.description}" />
+    </a>`)
+    .join("");
+gallery.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { 
+        captionsData: "alt",
+        captionDelay: 250
+    });
