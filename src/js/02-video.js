7ee2879d6f1ce50a003cac72c35726e12124af2e
Chumak Slavik <chumakslav@gmail.com>
2023-01-10 22:22:50 +0200

    log time function

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+const videoPlayer = document.querySelector("iframe#vimeo-player");
+console.log(videoPlayer);
 
+const player = new Player(videoPlayer);
+player.on("timeupdate", logTime);
+
+function logTime(data) {
+    console.log(data.seconds); 
+}
