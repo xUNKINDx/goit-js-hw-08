399785db1b7ab22d23e0fe3a926e631dd1fa5814
Chumak Slavik <chumakslav@gmail.com>
2023-01-10 22:49:45 +0200

    completed hw-08-02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,20 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-const markup = galleryItems.map(photo => 
-    `<a class="gallery__item" href="${photo.original}">
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    photo =>
+      `<a class="gallery__item" href="${photo.original}">
         <img class="gallery__image" src="${photo.preview}" alt="${photo.description}" />
-    </a>`)
-    .join("");
+    </a>`
+  )
+  .join('');
 gallery.insertAdjacentHTML('beforeend', markup);
 
-new SimpleLightbox('.gallery a', { 
-        captionsData: "alt",
-        captionDelay: 250
-    });
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,18 @@
 import Player from '@vimeo/player';
-const videoPlayer = document.querySelector("iframe#vimeo-player");
+import throttle from 'lodash.throttle';
+
+const videoPlayer = document.querySelector('iframe#vimeo-player');
 console.log(videoPlayer);
 
 const player = new Player(videoPlayer);
-player.on("timeupdate", logTime);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
-function logTime(data) {
-    console.log(data.seconds); 
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+function onTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data.seconds);
 }
